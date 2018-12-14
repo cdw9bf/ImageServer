7528a50de5b5ad7a165aa94c020820019896484c
Chris Wiens <cwiens1@gmail.com>
2018-12-13 23:31:27 -0500

    Initial Class for JDBC Connections

--- src/main/java/imageserver/database/ImageDAO.java
+++ src/main/java/imageserver/database/ImageDAO.java
@@ -1,4 +1,45 @@
 package imageserver.database;
 
+import org.postgresql.jdbc3.Jdbc3PoolingDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class ImageDAO {
+    private Jdbc3PoolingDataSource source;
+
+    ImageDAO() {
+        source = new Jdbc3PoolingDataSource();
+        source.setDataSourceName("A Data Source");
+        source.setServerName("localhost");
+        source.setDatabaseName("test");
+        source.setUser("testuser");
+        source.setPassword("testpassword");
+        source.setMaxConnections(10);
+    }
+
+    ImageDAO(Jdbc3PoolingDataSource source) {
+        this.source = source;
+    }
+
+    /**
+     *
+     * @param id
+     */
+    public void fetchImageMetadata(Integer id) {
+        Connection con = null;
+        try {
+            con = source.getConnection();
+            // use connection
+        } catch(SQLException e) {
+            // log error
+        } finally {
+            if(con != null) {
+                try {con.close();}catch(SQLException e) {}
+            }
+        }
+    }
+
+
+
 }
